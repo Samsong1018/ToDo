09969d132fa954fd12b6c38c7186ce4c1a8f6e13
Amos Horne <famousamosgames@gmail.com>
2020-07-30 11:32:33 -0700

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Amos Horne on 7/30/20.
+//  Copyright © 2020 FamousAmosGames. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
